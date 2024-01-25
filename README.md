82828f5eb96e7676f5c1bb9163d215e5df00e015
Thomas Traversié <thomas.traversie@gamil.com>
2024-01-25 16:08:03 +0100

    Handle Dedukti

--- README.md
+++ README.md
@@ -1,32 +1,48 @@
-# Double negation translation for higher-order Lambdapi proofs
+# Double negation translation for higher-order proofs in Dedukti and Lambdapi
 
-[Lambdapi](https://github.com/Deducteam/lambdapi) is a proof assitant based on the λΠ-calculus modulo, that is λ-calculus with dependent types and user-defined rewrite rules. It is a logical framework in which you can express different theories.
+[Dedukti](https://github.com/Deducteam/Dedukti) is a proof language based on the λΠ-calculus modulo, that is λ-calculus with dependent types and user-defined rewrite rules.
+
+[Lambdapi](https://github.com/Deducteam/lambdapi) is a proof assitant based on the λΠ-calculus modulo, which can check and output Dedukti proofs.
 
 Kuroda's translation adds double negations in front of each formulas and after each universal quanftifiers. A formula admits a classical proofs if and only if the translated formula admits an intuitionistic proof.
 
-This tool applies Kuroda's translation to Lambapi proofs written using natural deduction. It takes as input classical proofs, and returns the intuitionistic proofs of the translated theorems.
+This tool applies Kuroda's translation to Dedukti/Lambapi proofs written using natural deduction. It takes as input classical proofs, and returns the intuitionistic proofs of the translated theorems.
 
+## Requirements
 
-## How to use it?
-
-- Install [Lambdapi](https://github.com/Deducteam/lambdapi).
+- Install [Lambdapi](https://github.com/Deducteam/lambdapi) or [Dedukti](https://github.com/Deducteam/Dedukti).
 
 - Clone this repository:
 ```
 git clone https://github.com/thomastraversie/construkti/
+```
+
+
+## How to use it?
+
+- Enter this repository:
+```
 cd construkti
 ```
-- Write your proofs only using the connectors and quantifiers presented in `logic.lp`, and the natural deduction rules presented in `deduction.lp`. 
 
-- If you want to translate the proofs contained in the folder `folder` (that is in the directory `construkti`), run 
+- Write your proofs only using the connectors, quantifiers, and natural deduction rules presented in `logic.lp` or `logic.dk`. 
+
+- If you want to translate the Lambdapi file `file.lp` that depends on the module `Construkti.logic`, run 
+```
+bash translate.sh file.lp
+```
+The translated proofs are displayed in the file `file_c.lp`. You can test it with `test.lp`.
+
+- If you want to translate the Dedukti file `file.dk` such that the concatenation of `logic.dk` and `file.dk` typechecks, run 
+```
+bash translate.sh file.dk
+```
+The translated proofs are displayed in the file `file_c.dk`. You can test it with `test.dk`.
+
+- If you want to translate all the Lambdapi files of the folder `folder`, run 
 ```
 bash translate.sh folder
 ```
-It creates a new folder `folder_c`, that contains the translated proofs.
+The translated files are displayed in the folder `folder_c`.
 
 You can test it on the folder `library`, that contain some proofs in higher-order logic.
-
-
-## How does it work?
-
-`kuroda.lp` defines Kuroda's translation, i.e. the insertion of double negation before each formula and after each universal quantifier. Kuroda's translation relies on some properties, proved in `properties.lp`.
--- translate.sh
+++ translate.sh
@@ -1,31 +1,104 @@
 #!/bin/bash
 
-cp -r $1 $1_c
+extension="${1##*.}"
+filename="${1%.*}"
 
-sed -i 's/ Prf/ Prf ¬ ¬/g' ./$1_c/*.lp
-sed -i 's/∀/∀_c/g' ./$1_c/*.lp
-sed -i 's/imp_i /imp_i_c /g' ./$1_c/*.lp
-sed -i 's/imp_e /imp_e_c /g' ./$1_c/*.lp
-sed -i 's/and_i /and_i_c /g' ./$1_c/*.lp
-sed -i 's/and_e_l /and_e_l_c /g' ./$1_c/*.lp
-sed -i 's/and_e_r /and_e_r_c /g' ./$1_c/*.lp
-sed -i 's/or_i_r /or_i_r_c /g' ./$1_c/*.lp
-sed -i 's/or_i_l /or_i_l_c /g' ./$1_c/*.lp
-sed -i 's/or_e /or_e_c /g' ./$1_c/*.lp
-sed -i 's/neg_i /neg_i_c /g' ./$1_c/*.lp
-sed -i 's/neg_e /neg_e_c /g' ./$1_c/*.lp
-sed -i 's/top_i/top_i_c/g' ./$1_c/*.lp
-sed -i 's/bot_e /bot_e_c /g' ./$1_c/*.lp
-sed -i 's/fa_i /fa_i_c /g' ./$1_c/*.lp
-sed -i 's/fa_e /fa_e_c /g' ./$1_c/*.lp
-sed -i 's/ex_i /ex_i_c /g' ./$1_c/*.lp
-sed -i 's/ex_e /ex_e_c /g' ./$1_c/*.lp
-sed -i 's/absurd /absurd_c /g' ./$1_c/*.lp
 
-sed -i 's/apply /refine /g' ./$1_c/*.lp
+if [ $extension = "lp" ]
+then
+    new="${filename}_c.lp"
+    cp -r $1 $new
 
-sed -i '1s/^/require open Construkti.properties;\nrequire open Construkti.kuroda;\n/' ./$1_c/*.lp
+    sed -i 's/ Prf/ Prf ¬ ¬/g' $new
+    sed -i 's/∀/∀_c/g' $new
+    sed -i 's/imp_i /imp_i_c /g' $new
+    sed -i 's/imp_e /imp_e_c /g' $new
+    sed -i 's/and_i /and_i_c /g' $new
+    sed -i 's/and_e_l /and_e_l_c /g' $new
+    sed -i 's/and_e_r /and_e_r_c /g' $new
+    sed -i 's/or_i_r /or_i_r_c /g' $new
+    sed -i 's/or_i_l /or_i_l_c /g' $new
+    sed -i 's/or_e /or_e_c /g' $new
+    sed -i 's/neg_i /neg_i_c /g' $new
+    sed -i 's/neg_e /neg_e_c /g' $new
+    sed -i 's/top_i/top_i_c/g' $new
+    sed -i 's/bot_e /bot_e_c /g' $new
+    sed -i 's/fa_i /fa_i_c /g' $new
+    sed -i 's/fa_e /fa_e_c /g' $new
+    sed -i 's/ex_i /ex_i_c /g' $new
+    sed -i 's/ex_e /ex_e_c /g' $new
+    sed -i 's/absurd /absurd_c /g' $new
+    sed -i 's/apply /refine /g' $new
+    sed -i '1s/^/require open Construkti.properties;\nrequire open Construkti.kuroda;\n/' $new
 
-sed -i "s/Construkti.$1/Construkti.$1_c/g" ./$1_c/*.lp
+    echo "$1 has been translated"
 
-echo "Files translated"
+
+elif [ $extension = "dk" ]
+then
+    new="${filename}_c.dk"
+    tmp="tmp.dk"
+    cp -r $1 $tmp
+
+    sed -i 's/ Prf/ Prf_c/g' $tmp
+    sed -i 's/all/all_c/g' $tmp
+    sed -i 's/imp_i /imp_i_c /g' $tmp
+    sed -i 's/imp_e /imp_e_c /g' $tmp
+    sed -i 's/and_i /and_i_c /g' $tmp
+    sed -i 's/and_e_l /and_e_l_c /g' $tmp
+    sed -i 's/and_e_r /and_e_r_c /g' $tmp
+    sed -i 's/or_i_r /or_i_r_c /g' $tmp
+    sed -i 's/or_i_l /or_i_l_c /g' $tmp
+    sed -i 's/or_e /or_e_c /g' $tmp
+    sed -i 's/neg_i /neg_i_c /g' $tmp
+    sed -i 's/neg_e /neg_e_c /g' $tmp
+    sed -i 's/top_i/top_i_c/g' $tmp
+    sed -i 's/bot_e /bot_e_c /g' $tmp
+    sed -i 's/all_i /all_i_c /g' $tmp
+    sed -i 's/all_e /all_e_c /g' $tmp
+    sed -i 's/ex_i /ex_i_c /g' $tmp
+    sed -i 's/ex_e /ex_e_c /g' $tmp
+    sed -i 's/absurd /absurd_c /g' $tmp
+
+    cat logic.dk properties.dk kuroda.dk $tmp > $new
+    rm $tmp
+    echo "$1 has been translated"
+
+
+else
+    cp -r $1 $1_c
+
+    sed -i 's/ Prf/ Prf ¬ ¬/g' ./$1_c/*.lp
+    sed -i 's/∀/∀_c/g' ./$1_c/*.lp
+    sed -i 's/imp_i /imp_i_c /g' ./$1_c/*.lp
+    sed -i 's/imp_e /imp_e_c /g' ./$1_c/*.lp
+    sed -i 's/and_i /and_i_c /g' ./$1_c/*.lp
+    sed -i 's/and_e_l /and_e_l_c /g' ./$1_c/*.lp
+    sed -i 's/and_e_r /and_e_r_c /g' ./$1_c/*.lp
+    sed -i 's/or_i_r /or_i_r_c /g' ./$1_c/*.lp
+    sed -i 's/or_i_l /or_i_l_c /g' ./$1_c/*.lp
+    sed -i 's/or_e /or_e_c /g' ./$1_c/*.lp
+    sed -i 's/neg_i /neg_i_c /g' ./$1_c/*.lp
+    sed -i 's/neg_e /neg_e_c /g' ./$1_c/*.lp
+    sed -i 's/top_i/top_i_c/g' ./$1_c/*.lp
+    sed -i 's/bot_e /bot_e_c /g' ./$1_c/*.lp
+    sed -i 's/fa_i /fa_i_c /g' ./$1_c/*.lp
+    sed -i 's/fa_e /fa_e_c /g' ./$1_c/*.lp
+    sed -i 's/ex_i /ex_i_c /g' ./$1_c/*.lp
+    sed -i 's/ex_e /ex_e_c /g' ./$1_c/*.lp
+    sed -i 's/absurd /absurd_c /g' ./$1_c/*.lp
+
+    sed -i 's/apply /refine /g' ./$1_c/*.lp
+
+    sed -i '1s/^/require open Construkti.properties;\nrequire open Construkti.kuroda;\n/' ./$1_c/*.lp
+
+    sed -i "s/Construkti.$1/Construkti.$1_c/g" ./$1_c/*.lp
+
+    for file in $1/*.lp
+    do
+        echo "$file has been translated"
+    done
+
+    echo "The translated files are in the folder $1_c"
+    
+fi
