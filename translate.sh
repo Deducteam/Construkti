368eb5b0081a30e3d2ff027ac466b88b7f2e9d87
Thomas Traversié <thomas.traversie@gamil.com>
2024-02-13 17:57:32 +0100

    Modify order of arguments

--- translate.sh
+++ translate.sh
@@ -13,22 +13,22 @@ then
     sed -i 's/Prf /Prf_c /g' $tmp
     sed -i 's/Prf(/Prf_c(/g' $tmp
     sed -i 's/all /all_c /g' $tmp
-    sed -i 's/imp_intro /imp_intro_c /g' $tmp
-    sed -i 's/imp_elim /imp_elim_c /g' $tmp
-    sed -i 's/and_intro /and_intro_c /g' $tmp
-    sed -i 's/and_elim1 /and_elim1_c /g' $tmp
-    sed -i 's/and_elim2 /and_elim2_c /g' $tmp
-    sed -i 's/or_intro1 /or_intro1_c /g' $tmp
-    sed -i 's/or_intro2 /or_intro2_c /g' $tmp
-    sed -i 's/or_elim /or_elim_c /g' $tmp
-    sed -i 's/neg_intro /neg_intro_c /g' $tmp
-    sed -i 's/neg_elim /neg_elim_c /g' $tmp
-    sed -i 's/top_intro/top_intro_c/g' $tmp
-    sed -i 's/bot_elim /bot_elim_c /g' $tmp
-    sed -i 's/all_intro /all_intro_c /g' $tmp
-    sed -i 's/all_elim /all_elim_c /g' $tmp
-    sed -i 's/ex_intro /ex_intro_c /g' $tmp
-    sed -i 's/ex_elim /ex_elim_c /g' $tmp
+    sed -i 's/imp_i /imp_i_c /g' $tmp
+    sed -i 's/imp_e /imp_e_c /g' $tmp
+    sed -i 's/and_i /and_i_c /g' $tmp
+    sed -i 's/and_el /and_el_c /g' $tmp
+    sed -i 's/and_er /and_er_c /g' $tmp
+    sed -i 's/or_il /or_il_c /g' $tmp
+    sed -i 's/or_ir /or_ir_c /g' $tmp
+    sed -i 's/or_e /or_e_c /g' $tmp
+    sed -i 's/neg_i /neg_i_c /g' $tmp
+    sed -i 's/neg_e /neg_e_c /g' $tmp
+    sed -i 's/top_i/top_i_c/g' $tmp
+    sed -i 's/bot_e /bot_e_c /g' $tmp
+    sed -i 's/all_i /all_i_c /g' $tmp
+    sed -i 's/all_e /all_e_c /g' $tmp
+    sed -i 's/ex_i /ex_i_c /g' $tmp
+    sed -i 's/ex_e /ex_e_c /g' $tmp
     sed -i 's/excluded_middle /excluded_middle_c /g' $tmp
 
     cat kuroda.dk $tmp > "${tmp2}.dk"
@@ -50,15 +50,16 @@ then
     new="${filename}_c.lp"
     cp -r $1 $new
 
-    sed -i 's/ Prf/ Prf ¬ ¬/g' $new
+    sed -i 's/Prf /Prf ¬ ¬ /g' $new
+    sed -i 's/Prf(/Prf ¬ ¬(/g' $new
     sed -i 's/∀/∀_c/g' $new
     sed -i 's/imp_i /imp_i_c /g' $new
     sed -i 's/imp_e /imp_e_c /g' $new
     sed -i 's/and_i /and_i_c /g' $new
-    sed -i 's/and_e_l /and_e_l_c /g' $new
-    sed -i 's/and_e_r /and_e_r_c /g' $new
-    sed -i 's/or_i_r /or_i_r_c /g' $new
-    sed -i 's/or_i_l /or_i_l_c /g' $new
+    sed -i 's/and_el /and_el_c /g' $new
+    sed -i 's/and_er /and_er_c /g' $new
+    sed -i 's/or_il /or_il_c /g' $new
+    sed -i 's/or_ir /or_ir_c /g' $new
     sed -i 's/or_e /or_e_c /g' $new
     sed -i 's/neg_i /neg_i_c /g' $new
     sed -i 's/neg_e /neg_e_c /g' $new
@@ -78,15 +79,16 @@ then
 else
     cp -r $1 $1_c
 
-    sed -i 's/ Prf/ Prf ¬ ¬/g' ./$1_c/*.lp
+    sed -i 's/Prf /Prf ¬ ¬ /g' ./$1_c/*.lp
+    sed -i 's/Prf(/Prf ¬ ¬(/g' ./$1_c/*.lp
     sed -i 's/∀/∀_c/g' ./$1_c/*.lp
     sed -i 's/imp_i /imp_i_c /g' ./$1_c/*.lp
     sed -i 's/imp_e /imp_e_c /g' ./$1_c/*.lp
     sed -i 's/and_i /and_i_c /g' ./$1_c/*.lp
-    sed -i 's/and_e_l /and_e_l_c /g' ./$1_c/*.lp
-    sed -i 's/and_e_r /and_e_r_c /g' ./$1_c/*.lp
-    sed -i 's/or_i_r /or_i_r_c /g' ./$1_c/*.lp
-    sed -i 's/or_i_l /or_i_l_c /g' ./$1_c/*.lp
+    sed -i 's/and_el /and_el_c /g' ./$1_c/*.lp
+    sed -i 's/and_er /and_er_c /g' ./$1_c/*.lp
+    sed -i 's/or_il /or_il_c /g' ./$1_c/*.lp
+    sed -i 's/or_ir /or_ir_c /g' ./$1_c/*.lp
     sed -i 's/or_e /or_e_c /g' ./$1_c/*.lp
     sed -i 's/neg_i /neg_i_c /g' ./$1_c/*.lp
     sed -i 's/neg_e /neg_e_c /g' ./$1_c/*.lp
