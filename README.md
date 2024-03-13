673218de15bc695ce4eb0b1de986cb28560a8502
Thomas Traversié <thomas.traversie@gamil.com>
2024-03-13 17:44:27 +0100

    Code for replacing Prf_c and all_c

--- README.md
+++ README.md
@@ -1,36 +1,32 @@
-# Construkti, a double negation translator for higher-order proofs in Dedukti and Lambdapi
+# Construkti, a double negation translator for higher-order proofs in Dedukti
 
 [Dedukti](https://github.com/Deducteam/Dedukti) is a proof language based on the λΠ-calculus modulo theory, that is λ-calculus with dependent types and user-defined rewrite rules.
 
-[Lambdapi](https://github.com/Deducteam/lambdapi) is a proof assitant based on the λΠ-calculus modulo theory, which can check and output Dedukti proofs.
-
 Kuroda's translation adds double negations in front of each formulas and after each universal quanftifiers. A formula admits a classical proofs if and only if the translated formula admits an intuitionistic proof.
 
-This tool applies Kuroda's translation to Dedukti/Lambapi proofs written using natural deduction. It takes as input classical proofs, and returns the intuitionistic proofs of the translated theorems.
+This tool applies Kuroda's translation to Dedukti proofs written using natural deduction. It takes as input classical proofs, and returns the intuitionistic proofs of the translated theorems.
 
 
 ## Requirements
 
-- Install [Lambdapi](https://github.com/Deducteam/lambdapi) or [Dedukti](https://github.com/Deducteam/Dedukti).
+- Opam and OCaml
+
+- Install [Dedukti](https://github.com/Deducteam/Dedukti?tab=readme-ov-file#install-with-opam) with Opam.
+
+
+## How to use it?
 
 - Clone this repository:
 ```
 git clone https://github.com/thomastraversie/construkti/
 ```
 
-
-## How to use it?
-
 - Enter this repository:
 ```
 cd construkti
 ```
 
-- Write your proofs only using the connectors, quantifiers, and natural deduction rules presented in `logic.lp` or `logic.dk`. 
-
-
-
-#### Dedukti file
+- Write your proofs only using the connectors, quantifiers, and natural deduction rules presented in `logic.dk`. 
 
 If you want to translate the Dedukti file `file.dk` such that the concatenation of `logic.dk` and `file.dk` typechecks, run 
 ```
@@ -39,26 +35,6 @@ bash translate.sh file.dk
 The translated proofs are displayed in the file `file_c.dk`.
 
 
-
-#### Lambdapi file
-
-If you want to translate the Lambdapi file `file.lp` that depends on the module `Construkti.logic`, run 
-```
-bash translate.sh file.lp
-```
-The translated proofs are displayed in the file `file_c.lp`.
-
-
-
-#### Multiple Lambdapi files
-
-If you want to translate all the Lambdapi files of the folder `folder`, run 
-```
-bash translate.sh folder
-```
-The translated files are displayed in the folder `folder_c`.
-
-
 ### Benchmark
 
-You can test Construkti on `hol-lib.dk` and `hol-lib.lp`, that contain the proofs of theorems in propositional, first-order and higher-order logics, including classical theorems. After translation, `hol-lib_c.dk` and `hol-lib_c.lp` only contain intuitionistic proofs.
+You can test Construkti on `hol-lib.dk`, that contain the proofs of theorems in propositional, first-order and higher-order logics, including classical theorems. After translation, `hol-lib_c.dk` only contain intuitionistic proofs.
--- translate.sh
+++ translate.sh
@@ -3,10 +3,15 @@
 extension="${1##*.}"
 filename="${1%.*}"
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
 if [ $extension = "dk" ]
 then
     new="${filename}_c.dk"
     tmp="tmp.dk"
+    tmp2="tmp2.dk"
     cp -r $1 $tmp
 
     sed -i 's/Prf /Prf_c /g' $tmp
@@ -30,76 +35,19 @@ then
     sed -i 's/ex_e /ex_e_c /g' $tmp
     sed -i 's/excluded_middle /excluded_middle_c /g' $tmp
 
+    dune build
+    dune exec -- construkti --file $tmp > $tmp2
+    sed -i 's/{ //g' $tmp2
+    sed -i 's/ }//g' $tmp2
+    sed -i 's/tmp.//g' $tmp2
+    dk beautify $tmp2 > $tmp
+
     cat kuroda.dk $tmp > $new
-    rm $tmp
-    echo "$1 has been translated"
-    echo "[Warning] Apply the rewrites rules for Prf_c and all_c on the left-hand side of the rewrite rules" 
-
-
-elif [ $extension = "lp" ]
-then
-    new="${filename}_c.lp"
-    cp -r $1 $new
-
-    sed -i 's/Prf /Prf_c /g' $new
-    sed -i 's/Prf(/Prf_c(/g' $new
-    sed -i 's/∀/∀_c/g' $new
-    sed -i 's/imp_i /imp_i_c /g' $new
-    sed -i 's/imp_e /imp_e_c /g' $new
-    sed -i 's/and_i /and_i_c /g' $new
-    sed -i 's/and_el /and_el_c /g' $new
-    sed -i 's/and_er /and_er_c /g' $new
-    sed -i 's/or_il /or_il_c /g' $new
-    sed -i 's/or_ir /or_ir_c /g' $new
-    sed -i 's/or_e /or_e_c /g' $new
-    sed -i 's/neg_i /neg_i_c /g' $new
-    sed -i 's/neg_e /neg_e_c /g' $new
-    sed -i 's/top_i/top_i_c/g' $new
-    sed -i 's/bot_e /bot_e_c /g' $new
-    sed -i 's/all_i /all_i_c /g' $new
-    sed -i 's/all_e /all_e_c /g' $new
-    sed -i 's/ex_i /ex_i_c /g' $new
-    sed -i 's/ex_e /ex_e_c /g' $new
-    sed -i 's/excluded_middle /excluded_middle_c /g' $new
-    sed -i 's/apply /refine /g' $new
-    sed -i '1s/^/require open Construkti.properties;\nrequire open Construkti.kuroda;\n/' $new
-
-    echo "$1 has been translated"
-    echo "[Warning] Apply the rewrites rules for Prf_c and ∀_c on the left-hand side of the rewrite rules" 
+    rm $tmp $tmp2
+    
+    echo -e "${GREEN}[SUCCESS]${NOCOLOR} $1 was successfully translated into $new."
+    dk check $new
 
 else
-    cp -r $1 $1_c
-
-    sed -i 's/Prf /Prf_c /g' ./$1_c/*.lp
-    sed -i 's/Prf(/Prf_c(/g' ./$1_c/*.lp
-    sed -i 's/∀/∀_c/g' ./$1_c/*.lp
-    sed -i 's/imp_i /imp_i_c /g' ./$1_c/*.lp
-    sed -i 's/imp_e /imp_e_c /g' ./$1_c/*.lp
-    sed -i 's/and_i /and_i_c /g' ./$1_c/*.lp
-    sed -i 's/and_el /and_el_c /g' ./$1_c/*.lp
-    sed -i 's/and_er /and_er_c /g' ./$1_c/*.lp
-    sed -i 's/or_il /or_il_c /g' ./$1_c/*.lp
-    sed -i 's/or_ir /or_ir_c /g' ./$1_c/*.lp
-    sed -i 's/or_e /or_e_c /g' ./$1_c/*.lp
-    sed -i 's/neg_i /neg_i_c /g' ./$1_c/*.lp
-    sed -i 's/neg_e /neg_e_c /g' ./$1_c/*.lp
-    sed -i 's/top_i/top_i_c/g' ./$1_c/*.lp
-    sed -i 's/bot_e /bot_e_c /g' ./$1_c/*.lp
-    sed -i 's/all_i /all_i_c /g' ./$1_c/*.lp
-    sed -i 's/all_e /all_e_c /g' ./$1_c/*.lp
-    sed -i 's/ex_i /ex_i_c /g' ./$1_c/*.lp
-    sed -i 's/ex_e /ex_e_c /g' ./$1_c/*.lp
-    sed -i 's/excluded_middle /excluded_middle_c /g' ./$1_c/*.lp
-    sed -i 's/apply /refine /g' ./$1_c/*.lp
-    sed -i '1s/^/require open Construkti.properties;\nrequire open Construkti.kuroda;\n/' ./$1_c/*.lp
-    sed -i "s/Construkti.$1/Construkti.$1_c/g" ./$1_c/*.lp
-
-    for file in $1/*.lp
-    do
-        echo "$file has been translated"
-    done
-
-    echo "The translated files are in the folder $1_c"
-    echo "[Warning] Apply the rewrites rules for Prf_c and ∀_c on the left-hand side of the rewrite rules"
-    
+    echo -e "${RED}[ERROR]${NOCOLOR} $1 is not a Dedukti file."
 fi
